d92af5b45524f824cf716f704615a7ee71ca81cc
KitsuneAlex <alexh@karmakrafts.dev>
2024-02-05 22:59:25 +0100

    Add translations, creative mode tab & test block state for dock

--- src/main/java/io/karma/pda/PDAMod.java
+++ src/main/java/io/karma/pda/PDAMod.java
@@ -2,11 +2,16 @@ package io.karma.pda;
 
 import io.karma.pda.init.ModBlocks;
 import io.karma.pda.init.ModItems;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
  * @author Alexander Hinze
@@ -17,9 +22,26 @@ public class PDAMod {
     public static final String MODID = "pda";
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    // @formatter:off
+    private static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final RegistryObject<CreativeModeTab> TAB = TABS.register("main", () -> CreativeModeTab.builder()
+        .title(Component.translatable(String.format("itemGroup.%s", MODID)))
+        .icon(ModItems.pda.get()::getDefaultInstance)
+        .displayItems((params, output) -> {
+            ITEMS.getEntries().stream().map(RegistryObject::get).forEach(output::accept);
+        })
+        .build());
+    // @formatter:on
 
     static {
         ModBlocks.register(BLOCKS);
         ModItems.register(ITEMS);
+    }
+
+    public PDAMod() {
+        final var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(bus);
+        ITEMS.register(bus);
+        TABS.register(bus);
     }
 }
--- src/main/java/io/karma/pda/block/DockBlock.java
+++ src/main/java/io/karma/pda/block/DockBlock.java
@@ -1,13 +1,73 @@
 package io.karma.pda.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Alexander Hinze
  * @since 05/02/2024
  */
 public final class DockBlock extends Block {
+    public static final VoxelShape SHAPE = makeShape();
+
     public DockBlock() {
         super(Properties.of());
+    }
+
+    private static VoxelShape makeShape() {
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0.125, 0, 0.3125, 0.875, 0.125, 0.6875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0, 0.125, 0.875, 0.25, 0.3125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.125, 0.3125, 0.875, 0.1875, 0.375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.125, 0.625, 0.875, 0.1875, 0.6875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.125, 0.375, 0.1875, 0.1875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.8125, 0.125, 0.375, 0.875, 0.1875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.25, 0.125, 0.375, 0.3125, 0.15625, 0.4375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0.125, 0.375, 0.4375, 0.15625, 0.4375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.5, 0.125, 0.375, 0.5625, 0.15625, 0.4375), BooleanOp.OR);
+        return shape;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isPathfindable(final @NotNull BlockState state, final @NotNull BlockGetter world,
+                                  final @NotNull BlockPos pos, final @NotNull PathComputationType type) {
+        return false;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public @NotNull VoxelShape getOcclusionShape(final @NotNull BlockState state, final @NotNull BlockGetter world,
+                                                 final @NotNull BlockPos pos) {
+        return SHAPE;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public @NotNull VoxelShape getShape(final @NotNull BlockState state, final @NotNull BlockGetter world,
+                                        final @NotNull BlockPos pos, final @NotNull CollisionContext context) {
+        return SHAPE;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public @NotNull VoxelShape getVisualShape(final @NotNull BlockState state, final @NotNull BlockGetter world,
+                                              final @NotNull BlockPos pos, final @NotNull CollisionContext context) {
+        return getShape(state, world, pos, context);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public @NotNull VoxelShape getCollisionShape(final @NotNull BlockState state, final @NotNull BlockGetter world,
+                                                 final @NotNull BlockPos pos, final @NotNull CollisionContext context) {
+        return getShape(state, world, pos, context);
     }
 }
