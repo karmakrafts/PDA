7884c8ec1ec03b0fb74bb2c16604fc26897bc9ac
KitsuneAlex <alexh@karmakrafts.dev>
2024-02-06 16:53:06 +0100

    Implement horizontal rotation for PDA item when shift-right-clicking

--- src/main/java/io/karma/pda/item/PDAItem.java
+++ src/main/java/io/karma/pda/item/PDAItem.java
@@ -3,9 +3,15 @@ package io.karma.pda.item;
 import io.karma.pda.PDAMod;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Alexander Hinze
@@ -36,5 +42,22 @@ public final class PDAItem extends Item {
                 });
             return null;
         });
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(final @NotNull Level world, final @NotNull Player player,
+                                                           final @NotNull InteractionHand hand) {
+        final var stack = player.getItemInHand(hand);
+        if (!world.isClientSide) {
+            if (player.isShiftKeyDown()) {
+                final var tag = stack.getOrCreateTag();
+                tag.putBoolean(TAG_IS_HORIZONTAL,
+                    !tag.contains(TAG_IS_HORIZONTAL) || !tag.getBoolean(TAG_IS_HORIZONTAL));
+            }
+            else {
+                // open menu..
+            }
+        }
+        return InteractionResultHolder.success(stack);
     }
 }
