21a324329757034fa6e4bce4495d0b603a6017e1
KitsuneAlex <alexh@karmakrafts.dev>
2024-04-10 22:09:31 +0200

    Fix illegal forward reference caused by formatter

--- src/main/java/io/karma/pda/api/common/app/DefaultApps.java
+++ src/main/java/io/karma/pda/api/common/app/DefaultApps.java
@@ -12,9 +12,10 @@ import net.minecraftforge.registries.ObjectHolder;
  * @since 18/02/2024
  */
 public final class DefaultApps {
+    private static final String REGISTRY_NAME = Constants.MODID + ":apps";
+
     @ObjectHolder(value = Constants.MODID + ":launcher", registryName = REGISTRY_NAME)
     public static final AppType<?> LAUNCHER = null;
-    private static final String REGISTRY_NAME = Constants.MODID + ":apps";
 
     // @formatter:off
     private DefaultApps() {}
