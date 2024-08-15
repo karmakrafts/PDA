93828d2f58373bf0603550b60f23748ae687bbc6
KitsuneAlex <alexh@karmakrafts.dev>
2024-08-15 02:50:36 +0200

    Fix constness in GLSL shaders to avoid compilation errors on Windows

--- src/main/resources/assets/pda/shaders/include/crt.glsl
+++ src/main/resources/assets/pda/shaders/include/crt.glsl
@@ -2,15 +2,15 @@
  * Copyright (C) 2024 Karma Krafts & associates
  */
 
-vec4 crtFadeOut(const vec2 uv, const vec2 size, const sampler2D tex, const float time, const float bufferFactor,
-const float scaleSpeed, const vec4 fadeColor) {
-    const float pixelScale = (size.y - 1.0) / size.y;
-    const float s = 1.0 / scaleSpeed + 1.0;
-    const float t = clamp(mod(time * 4.0, s + bufferFactor * 2.0) - bufferFactor, 0.0, s);
-    const float st = clamp(t * scaleSpeed, 0.0, pixelScale);
-    const float ft = clamp(t - pixelScale / scaleSpeed, 0.0, 1.0);
+vec4 crtFadeOut(vec2 uv, vec2 size, sampler2D tex, float time, float bufferFactor,
+float scaleSpeed, vec4 fadeColor) {
+    float pixelScale = (size.y - 1.0) / size.y;
+    float s = 1.0 / scaleSpeed + 1.0;
+    float t = clamp(mod(time * 4.0, s + bufferFactor * 2.0) - bufferFactor, 0.0, s);
+    float st = clamp(t * scaleSpeed, 0.0, pixelScale);
+    float ft = clamp(t - pixelScale / scaleSpeed, 0.0, 1.0);
     // @formatter:off
-    const vec4 texColor = texture(tex, vec2(
+    vec4 texColor = texture(tex, vec2(
         (uv.x - 0.5) * (1.0 - st) + 0.5,
         (uv.y - 0.5) / (1.0 - st) + 0.5
     ));
@@ -21,7 +21,7 @@ const float scaleSpeed, const vec4 fadeColor) {
     // @formatter:on
 }
 
-vec4 crtFadeIn(const vec2 uv, const vec2 size, const sampler2D tex, const float time, const float bufferFactor,
-const float scaleSpeed, const vec4 fadeColor) {
+vec4 crtFadeIn(vec2 uv, vec2 size, sampler2D tex, float time, float bufferFactor,
+float scaleSpeed, vec4 fadeColor) {
     return crtFadeOut(uv, size, tex, -time, bufferFactor, scaleSpeed, fadeColor);
 }
--- src/main/resources/assets/pda/shaders/include/dither.glsl
+++ src/main/resources/assets/pda/shaders/include/dither.glsl
@@ -15,13 +15,13 @@ const float DEFAULT_DITHER_COLOR_FACTOR = 16.0;
 const float DEFAULT_DITHER_FACTOR = 0.005;
 const int DITHER_KERNEL_SIZE = 4;
 
-vec4 dither(const mat4 kernel, const vec4 color, const vec2 coord, const float colorFactor, const float factor) {
-    const int xIndex = int(coord.x) % DITHER_KERNEL_SIZE;
-    const int yIndex = int(coord.y) % DITHER_KERNEL_SIZE;
-    const vec3 result = color.rgb + kernel[xIndex][yIndex] * factor;
+vec4 dither(mat4 kernel, vec4 color, vec2 coord, float colorFactor, float factor) {
+    int xIndex = int(coord.x) % DITHER_KERNEL_SIZE;
+    int yIndex = int(coord.y) % DITHER_KERNEL_SIZE;
+    vec3 result = color.rgb + kernel[xIndex][yIndex] * factor;
     return vec4(floor(result * colorFactor) / colorFactor, color.a);
 }
 
-vec4 dither(const vec4 color, const vec2 coord) {
+vec4 dither(vec4 color, vec2 coord) {
     return dither(DEFAULT_DITHER_KERNEL, color, coord, DEFAULT_DITHER_COLOR_FACTOR, DEFAULT_DITHER_FACTOR);
 }
